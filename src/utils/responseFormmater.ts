e7d5ff0cecc4344b312606a91a3e81095a52907c
DongHyun Kim <developerforus@gmail.com>
2022-10-10 13:32:53 +0900

    request 프로퍼티 추가

--- src/utils/responseFormmater.ts
+++ src/utils/responseFormmater.ts
@@ -1,6 +1,16 @@
 import { Request } from "express";
 
-export const responseFormagger = (req: Request, response: any, statusCode: number) => {
-    req.responseObject = response;
-    req.statusCode = statusCode
+// userid를 전달하기 위해 Request user 프로퍼티 추가
+declare global {
+  namespace Express {
+    interface Request {
+      responseObject?: any;
+      statusCode?: number;
+    }
+  }
 }
+
+export const responseFormagger = (req: Request, response: any, statusCode: number) => {
+  req.responseObject = response;
+  req.statusCode = statusCode;
+};
