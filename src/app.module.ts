6455a1d03b0d002c3f726119164101f3089a04e4
YeonSeok-Song <dustjr1005@naver.com>
2022-10-23 18:20:47 +0900

    feature : MongoDB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { WebsiteModule } from './website/website.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, WebsiteModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'product' ? '.product.env' : '.develop.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    WebsiteModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
