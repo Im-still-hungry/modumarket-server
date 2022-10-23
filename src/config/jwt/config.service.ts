eb32ee553e3be08559927013f2e1baf2a0a0e672
parkjungyoon <tbr06057@naver.com>
2022-10-23 21:35:46 +0900

    [feat] add config jwt

---
+++ src/config/jwt/config.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+/**
+ * Service dealing with app config based operations.
+ *
+ * @class
+ */
+@Injectable()
+export class JwtConfigService {
+  constructor(private configService: ConfigService) {}
+
+  get accessSecretKey(): string {
+    return this.configService.get<string>('jwt.accessSecretKey');
+  }
+  get accessExpiresIn(): string {
+    return this.configService.get<string>('jwt.accessExpiresIn');
+  }
+}
